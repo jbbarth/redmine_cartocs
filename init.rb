43bca9e702554977e4e4e22c356845ce736b2b79
jbbarth <jeanbaptiste.barth@gmail.com>
2011-01-07 12:52:17 +0100

    Initial commit

---
+++ init.rb
@@ -0,0 +1,10 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_cartocs do
+  name 'Redmine Cartocs plugin'
+  author 'Jean-Baptiste BARTH'
+  description 'Cartocs integration'
+  author_url 'mailto:jeanbaptiste.barth@gmail.com'
+  requires_redmine :version_or_higher => '1.0.5'
+  version '0.1'
+end
