b893e18098b72eb3e54b85a3b5bf81cd7cdb24c0
jbbarth <jeanbaptiste.barth@gmail.com>
2012-09-26 13:47:34 +0200

    Consistency with my other plugins

--- init.rb
+++ init.rb
@@ -3,8 +3,9 @@ require 'redmine_cartocs/hooks'
 
 Redmine::Plugin.register :redmine_cartocs do
   name 'Redmine Cartocs plugin'
-  author 'Jean-Baptiste BARTH'
   description 'Cartocs integration'
+  url 'https://github.com/jbbarth/redmine_cartocs'
+  author 'Jean-Baptiste BARTH'
   author_url 'mailto:jeanbaptiste.barth@gmail.com'
   requires_redmine :version_or_higher => '2.0.0'
   version '0.2'
